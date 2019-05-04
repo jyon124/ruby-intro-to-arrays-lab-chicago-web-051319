d2a30d611ec10a4361b4b1dfe9bfa484cc663c34
Jooyon Won <jyon1204@gmail.com>
2019-05-04 01:12:12 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+instantiate_new_array = []
+return instantiate_new_array
+end
+
+def array_with_two_elements
+  new_array = ["Chloe", "Luna"]
+  return new_array
+end
+
+def first_element(animal)
+  cat = "Welcome to New York"
+  animal = cat
+      return cat
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def last_element_with_array_methods(array)
+return array.pop
+end
+
+def first_element_with_array_methods(array)
+return array.shift
+end
+
+def length_of_array(array)
+return array.length;
+end
